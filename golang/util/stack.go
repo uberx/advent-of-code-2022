0ce6bdfed3a8b5b14d3ea31be3175990a5382890
Ani Madabhushi <anirudhroxrulz@gmail.com>
2022-12-07 00:49:10 -0800

    Adding a generic stack data structure util

---
+++ golang/util/stack.go
@@ -0,0 +1,23 @@
+package util
+
+type Stack[T any] struct {
+	elements []T
+}
+
+func (s *Stack[T]) IsEmpty() bool {
+	return len(s.elements) == 0
+}
+
+func (s *Stack[T]) Push(element T) {
+	s.elements = append(s.elements, element)
+}
+
+func (s *Stack[T]) Pop() (element T, popped bool) {
+	if !s.IsEmpty() {
+		index := len(s.elements) - 1.
+		element = s.elements[index]
+		s.elements = s.elements[:index]
+		popped = true
+	}
+	return element, popped
+}
